dcbecbe8bf857cd98303c03d807c016a708a22b5
Dylan Turnbull <dylanturn@gmail.com>
2021-01-16 15:55:04 -0600

    added the code stuff

--- OxideRustEsLogger.cs
+++ OxideRustEsLogger.cs
@@ -1,81 +1,314 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
 using System;
-using System.Reflection;
-using System.Data;
 using UnityEngine;
-using Oxide.Core;
-using Oxide.Core.Configuration;
-using Oxide.Core.Logging;
-using Oxide.Core.Plugins;
+using UnityEngine.Networking;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Oxide.Plugins
 {
-    [Info("HelloWord", "igor", 1.0)]
+    [Info("OxideRustEsLogger", "RedSys", 1.0)]
+    [Description("Logs player actions.")]
     class OxideRustEsLogger : RustPlugin
     {
         // Called after the player object is created, but before the player has spawned
-        void OnPlayerConnected(BasePlayer player)
-        {
-            Puts("OnPlayerConnected works!");
+        void OnPlayerConnected(BasePlayer player) {
+            try
+            {
+                SendEventLog(JsonUtility.ToJson(new PlayerBaseEventLogEntry(player, "OnPlayerConnected")));
+            }
+            catch (Exception error)
+            {
+                LogToFile("es_logger.log", $"[{DateTime.Now}] ERROR - OnPlayerConnected - {error.Message}", this);
+            }
         }
 
         //  Called after the player has disconnected from the server
-        void OnPlayerDisconnected(BasePlayer player, string reason)
-        {
-            Puts("OnPlayerDisconnected works!");
+        void OnPlayerDisconnected(BasePlayer player, string reason) {
+            try
+            {
+                SendEventLog(JsonUtility.ToJson(new PlayerBaseEventLogEntry(player, "OnPlayerDisconnected")));
+            }
+            catch (Exception error)
+            {
+                LogToFile("es_logger.log", $"[{DateTime.Now}] ERROR - OnPlayerDisconnected - {error.Message}", this);
+            }
         }
 
         // Called when the player sends chat to the server
-        object OnPlayerChat(BasePlayer player, string message, Chat.ChatChannel channel)
-        {
-            Puts("OnPlayerChat works!");
-            return null;
-        }
-
-        // Called when the player triggers an anti-hack violation
-        object OnPlayerViolation(BasePlayer player, AntiHackType type, float amount)
-        {
-            Puts("OnPlayerViolation works!");
-            return null;
-        }
-
-        // Useful for modifying an attack before it goes out hitInfo.HitEntity should be the
-        void OnPlayerAttack(BasePlayer attacker, HitInfo info)
-        {
-            Puts("OnPlayerAttack works!");
-        }
-
-        // Called when the player is about to die. HitInfo may be null sometimes
-        object OnPlayerDeath(BasePlayer player, HitInfo info)
-        {
-            Puts("OnPlayerDeath works!");
+        object OnPlayerChat(BasePlayer player, string message, ConVar.Chat.ChatChannel channel) {
+            // PlayerChatEventLogEntry
+            try
+            {
+                SendEventLog(JsonUtility.ToJson(new PlayerChatEventLogEntry(player, message)));
+            }
+            catch (Exception error)
+            {
+                LogToFile("es_logger.log", $"[{DateTime.Now}] ERROR - OnPlayerChat - {error.Message}", this);
+            }
             return null;
         }
 
         // Called when the player starts looting another player
         void OnLootPlayer(BasePlayer player, BasePlayer target)
         {
-            Puts("OnLootPlayer works!");
+            try
+            {
+                SendEventLog(JsonUtility.ToJson(new PlayerLootEventLogEntry(player, target)));
+            }
+            catch (Exception error)
+            {
+                LogToFile("es_logger.log", $"[{DateTime.Now}] ERROR - OnLootPlayer - {error.Message}", this);
+            }
         }
 
-        /*
-         *  Base Event
+        // Useful for modifying an attack before it goes out hitInfo.HitEntity should be the
+        void OnPlayerAttack(BasePlayer attacker, HitInfo info) {
+            try
             {
-              "steam_id": 5555555555555,
-              "player_name": "fungus",
-              "action": "OnFooAction",
-              "data": {},
-              "timestamp": 3483476648,
-              "location": {
-                "x": 324,
-                "y": 564,
-                "z": 987
-              }
+                SendEventLog(JsonUtility.ToJson(new PlayerAttackEventLogEntry(attacker, info)));
             }
-         */
-        void SendEventLog(BasePlayer player, string action, Hash<string,string>data)
-        {
+            catch (Exception error)
+            {
+                LogToFile("es_logger.log", $"[{DateTime.Now}] ERROR - OnPlayerAttack - {error.Message}", this);
+            }
+        }
 
+        // Called when the player is about to die. HitInfo may be null sometimes
+        object OnPlayerDeath(BasePlayer player, HitInfo info) {
+            try
+            {
+                SendEventLog(JsonUtility.ToJson(new PlayerDeathEventLogEntry(player, info)));
+            }
+            catch (Exception error)
+            {
+                LogToFile("es_logger.log", $"[{DateTime.Now}] ERROR - OnPlayerDeath - {error.Message}", this);
+            }
+            return null;
+        }
+
+        IEnumerator SendEventLog(string logMsg) {
+
+            string identity = ConVar.Server.identity;
+            string suffix = getEsIndexSuffix();
+            string esIndex = String.Format("{0}-{1}", identity, suffix);
+            string esHost = "";
+            string esPort = "";
+            string esUsername = "";
+            string esPassword = "";
+            string esUri = String.Format("{0}:{1}/{2}",esHost,esPort,esIndex);
+            string esAuth = String.Format("{0}:{1}", esUsername, esPassword);
+
+            UnityWebRequest webRequest = UnityWebRequest.Post(esUri, logMsg);
+            webRequest.SetRequestHeader("Content-Type", "application/json");
+            webRequest.SetRequestHeader("Authorization", Base64Encode(esAuth));
+            webRequest.certificateHandler = new AcceptPinnedCerts();
+
+            yield return webRequest.SendWebRequest();
+            if (webRequest.isNetworkError || webRequest.isHttpError) {
+                LogToFile("es_logger.log", $"[{DateTime.Now}] ERROR {webRequest.error}", this);
+            }
+        }
+
+        public static string Base64Encode(string plainText) {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        string getEsIndexSuffix() {
+            DateTime dateTime = DateTime.UtcNow;
+            return String.Format("{0}-{1}", dateTime.Year, GetIso8601WeekOfYear(dateTime));
+        }
+
+        // Nabbed from: https://stackoverflow.com/questions/11154673/get-the-correct-week-number-of-a-given-date
+        public static int GetIso8601WeekOfYear(DateTime time) {
+            DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday) {
+                time = time.AddDays(3);
+            }
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+    }
+
+    [Serializable]
+    public class PlayerDeathEventLogEntry : PlayerBaseEventLogEntry
+    {
+        public string target_ip_address;
+        public ulong target_steam_id;
+        public string target_name;
+        public ulong target_team_id;
+        public string target_team_name;
+        public float target_health;
+        public float target_heartrate;
+        public float target_location_x;
+        public float target_location_y;
+        public float target_location_z;
+        public bool hit_info_null;
+        public string weapon_name;
+        public bool did_gather;
+        public bool is_projectile;
+        public PlayerDeathEventLogEntry(BasePlayer player, HitInfo info) : base(player, "OnPlayerDeath") {
+            if (info != null){
+                hit_info_null = false;
+                weapon_name = info.Weapon.name;
+                did_gather = info.DidGather;
+                is_projectile = info.IsProjectile();
+            } else {
+                hit_info_null = true;
+                weapon_name = "";
+                did_gather = false;
+                is_projectile = false;
+            }
+        }
+    }
+
+    [Serializable]
+    public class PlayerAttackEventLogEntry : PlayerBaseEventLogEntry
+    {
+        public string target_ip_address;
+        public ulong target_steam_id;
+        public string target_name;
+        public ulong target_team_id;
+        public string target_team_name;
+        public float target_health;
+        public float target_heartrate;
+        public float target_location_x;
+        public float target_location_y;
+        public float target_location_z;
+        public bool hit_info_null;
+        public string weapon_name;
+        public bool did_gather;
+        public bool is_projectile;
+        public PlayerAttackEventLogEntry(BasePlayer player, HitInfo info) : base(player, "OnPlayerAttack") {
+
+            BasePlayer targetPlayer = info.HitEntity.GetComponent<BasePlayer>();
+
+            string playerAddress = targetPlayer.net.connection.ipaddress;
+            target_ip_address = playerAddress.Substring(0, playerAddress.LastIndexOf(":"));
+            target_steam_id = targetPlayer.userID;
+            target_name = targetPlayer.displayName;
+            target_team_id = targetPlayer.Team.teamID;
+            target_team_name = targetPlayer.Team.teamName;
+            target_health = targetPlayer.health;
+            target_heartrate = targetPlayer.metabolism.heartrate.lastValue;
+            target_location_x = targetPlayer.transform.position.x;
+            target_location_y = targetPlayer.transform.position.y;
+            target_location_z = targetPlayer.transform.position.z;
+
+            if (info != null) {
+                hit_info_null = false;
+                weapon_name = info.Weapon.name;
+                did_gather = info.DidGather;
+                is_projectile = info.IsProjectile();
+            } else {
+                hit_info_null = true;
+                weapon_name = "";
+                did_gather = false;
+                is_projectile = false;
+            }
+        }
+    }
+
+    [Serializable]
+    public class PlayerLootEventLogEntry : PlayerBaseEventLogEntry {
+
+        public string target_ip_address;
+        public ulong target_steam_id;
+        public string target_name;
+        public ulong target_team_id;
+        public string target_team_name;
+        public float target_health;
+        public float target_heartrate;
+        public float target_location_x;
+        public float target_location_y;
+        public float target_location_z;
+
+        public PlayerLootEventLogEntry(BasePlayer player, BasePlayer targetPlayer) : base(player, "OnLootPlayer") {
+
+            string playerAddress = targetPlayer.net.connection.ipaddress;
+
+            target_ip_address = playerAddress.Substring(0, playerAddress.LastIndexOf(":"));
+            target_steam_id = targetPlayer.userID;
+            target_name = targetPlayer.displayName;
+            target_team_id = targetPlayer.Team.teamID;
+            target_team_name = targetPlayer.Team.teamName;
+            target_health = targetPlayer.health;
+            target_heartrate = targetPlayer.metabolism.heartrate.lastValue;
+            target_location_x = targetPlayer.transform.position.x;
+            target_location_y = targetPlayer.transform.position.y;
+            target_location_z = targetPlayer.transform.position.z;
+        }
+    }
+
+    [Serializable]
+    public class PlayerChatEventLogEntry : PlayerBaseEventLogEntry {
+        public string message;
+        public PlayerChatEventLogEntry(BasePlayer player, string message) : base(player, "OnPlayerChat") {
+            this.message = message;
+        }
+    }
+
+    [Serializable]
+    public class PlayerBaseEventLogEntry {
+        // We're using snake_case because this will be serialized to JSON.
+        public int timestamp;
+        public string ip_address;
+        public ulong steam_id;
+        public string name;
+        public ulong team_id;
+        public string team_name;
+        public float health;
+        public float heartrate;
+        public float location_x;
+        public float location_y;
+        public float location_z;
+        public string hook_name;
+
+        public PlayerBaseEventLogEntry(BasePlayer player, string hookName) {
+
+            string playerAddress = player.net.connection.ipaddress;
+
+            timestamp = getEpoch();
+            ip_address = playerAddress.Substring(0, playerAddress.LastIndexOf(":"));
+            steam_id = player.userID;
+            name = player.displayName;
+            team_id = player.Team.teamID;
+            team_name = player.Team.teamName;
+            health = player.health;
+            heartrate = player.metabolism.heartrate.lastValue;
+            location_x = player.transform.position.x;
+            location_y = player.transform.position.y;
+            location_z = player.transform.position.z;
+            hook_name = hookName;
+        }
+
+        int getEpoch() {
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            return (int)t.TotalSeconds;
+        }
+    }
+
+    // Based on https://www.owasp.org/index.php/Certificate_and_Public_Key_Pinning#.Net
+    class AcceptPinnedCerts : CertificateHandler
+    {
+        // Encoded RSAPublicKey
+        private static string PUB_KEY = "30818902818100C4A06B7B52F8D17DC1CCB47362" +
+            "C64AB799AAE19E245A7559E9CEEC7D8AA4DF07CB0B21FDFD763C63A313A668FE9D764E" +
+            "D913C51A676788DB62AF624F422C2F112C1316922AA5D37823CD9F43D1FC54513D14B2" +
+            "9E36991F08A042C42EAAEEE5FE8E2CB10167174A359CEBF6FACC2C9CA933AD403137EE" +
+            "2C3F4CBED9460129C72B0203010001";
+
+        protected override bool ValidateCertificate(byte[] certificateData)
+        {
+            return true;
+
+            X509Certificate2 certificate = new X509Certificate2(certificateData);
+            string pk = certificate.GetPublicKeyString();
+            if (pk.Equals(PUB_KEY))
+                return true;
+
+            // Bad dog
+            return false;
         }
     }
 }
