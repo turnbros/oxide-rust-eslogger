f7e0a938e8384d0c1ba653e82aa82e374708a54c
Dylan Turnbull <dylanturn@gmail.com>
2021-01-16 19:12:51 -0600

    updates

--- OxideRustEsLogger.cs
+++ OxideRustEsLogger.cs
@@ -5,11 +5,10 @@ using Newtonsoft.Json;
 using UnityEngine.Networking;
 using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
-using static Facepunch.Report;
 
 namespace Oxide.Plugins
 {
-    [Info("OxideRustEsLogger", "RedSys", 1.4)]
+    [Info("OxideRustEsLogger", "RedSys", 1.5)]
     [Description("Logs player actions.")]
     class OxideRustEsLogger : RustPlugin
     {
@@ -169,173 +168,189 @@ namespace Oxide.Plugins
             }
             return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
         }
-    }
 
-    [Serializable]
-    public class PlayerDeathEventLogEntry : PlayerBaseEventLogEntry
-    {
-        public string target_ip_address;
-        public ulong target_steam_id;
-        public string target_name;
-        public float target_health;
-        public float target_heartrate;
-        public float target_location_x;
-        public float target_location_y;
-        public float target_location_z;
-        public bool hit_info_null;
-        public string weapon_name;
-        public bool did_gather;
-        public bool is_projectile;
-        public PlayerDeathEventLogEntry(BasePlayer player, HitInfo info) : base(player, "OnPlayerDeath") {
-            if (info != null){
-                hit_info_null = false;
-                weapon_name = info.Weapon.name;
-                did_gather = info.DidGather;
-                is_projectile = info.IsProjectile();
-            } else {
-                hit_info_null = true;
-                weapon_name = "";
-                did_gather = false;
-                is_projectile = false;
-            }
-        }
-    }
-
-    [Serializable]
-    public class PlayerAttackEventLogEntry : PlayerBaseEventLogEntry
-    {
-        public string target_ip_address;
-        public ulong target_steam_id;
-        public string target_name;
-        public float target_health;
-        public float target_heartrate;
-        public float target_location_x;
-        public float target_location_y;
-        public float target_location_z;
-        public bool hit_info_null;
-        public string weapon_name;
-        public bool did_gather;
-        public bool is_projectile;
-        public PlayerAttackEventLogEntry(BasePlayer player, HitInfo info) : base(player, "OnPlayerAttack") {
-
-            BasePlayer targetPlayer = info.HitEntity.GetComponent<BasePlayer>();
-
-            string playerAddress = targetPlayer.net.connection.ipaddress;
-            target_ip_address = playerAddress.Substring(0, playerAddress.LastIndexOf(":"));
-            target_steam_id = targetPlayer.userID;
-            target_name = targetPlayer.displayName;
-            target_health = targetPlayer.health;
-            target_heartrate = targetPlayer.metabolism.heartrate.lastValue;
-            target_location_x = targetPlayer.transform.position.x;
-            target_location_y = targetPlayer.transform.position.y;
-            target_location_z = targetPlayer.transform.position.z;
-
-            if (info != null) {
-                hit_info_null = false;
-                weapon_name = info.Weapon.name;
-                did_gather = info.DidGather;
-                is_projectile = info.IsProjectile();
-            } else {
-                hit_info_null = true;
-                weapon_name = "";
-                did_gather = false;
-                is_projectile = false;
-            }
-        }
-    }
-
-    [Serializable]
-    public class PlayerLootEventLogEntry : PlayerBaseEventLogEntry {
-
-        public string target_ip_address;
-        public ulong target_steam_id;
-        public string target_name;
-        public float target_health;
-        public float target_heartrate;
-        public float target_location_x;
-        public float target_location_y;
-        public float target_location_z;
-
-        public PlayerLootEventLogEntry(BasePlayer player, BasePlayer targetPlayer) : base(player, "OnLootPlayer") {
-
-            string playerAddress = targetPlayer.net.connection.ipaddress;
-
-            target_ip_address = playerAddress.Substring(0, playerAddress.LastIndexOf(":"));
-            target_steam_id = targetPlayer.userID;
-            target_name = targetPlayer.displayName;
-            target_health = targetPlayer.health;
-            target_heartrate = targetPlayer.metabolism.heartrate.lastValue;
-            target_location_x = targetPlayer.transform.position.x;
-            target_location_y = targetPlayer.transform.position.y;
-            target_location_z = targetPlayer.transform.position.z;
-        }
-    }
-
-    [Serializable]
-    public class PlayerChatEventLogEntry : PlayerBaseEventLogEntry {
-        public string message;
-        public PlayerChatEventLogEntry(BasePlayer player, string message) : base(player, "OnPlayerChat") {
-            this.message = message;
-        }
-    }
-
-    [Serializable]
-    public class PlayerBaseEventLogEntry {
-        // We're using snake_case because this will be serialized to JSON.
-        public int timestamp;
-        public string ip_address;
-        public ulong steam_id;
-        public string name;
-        public float health;
-        public float heartrate;
-        public float location_x;
-        public float location_y;
-        public float location_z;
-        public string hook_name;
-
-        public PlayerBaseEventLogEntry(BasePlayer player, string hookName) {
-
-            string playerAddress = player.net.connection.ipaddress;
-
-            timestamp = getEpoch();
-            ip_address = playerAddress.Substring(0, playerAddress.LastIndexOf(":"));
-            steam_id = player.userID;
-            name = player.displayName;
-            health = player.health;
-            heartrate = player.metabolism.heartrate.lastValue;
-            location_x = player.transform.position.x;
-            location_y = player.transform.position.y;
-            location_z = player.transform.position.z;
-            hook_name = hookName;
-        }
-
-        int getEpoch() {
-            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
-            return (int)t.TotalSeconds;
-        }
-    }
-
-    // Based on https://www.owasp.org/index.php/Certificate_and_Public_Key_Pinning#.Net
-    class AcceptPinnedCerts : CertificateHandler
-    {
-        // Encoded RSAPublicKey
-        private static string PUB_KEY = "30818902818100C4A06B7B52F8D17DC1CCB47362" +
-            "C64AB799AAE19E245A7559E9CEEC7D8AA4DF07CB0B21FDFD763C63A313A668FE9D764E" +
-            "D913C51A676788DB62AF624F422C2F112C1316922AA5D37823CD9F43D1FC54513D14B2" +
-            "9E36991F08A042C42EAAEEE5FE8E2CB10167174A359CEBF6FACC2C9CA933AD403137EE" +
-            "2C3F4CBED9460129C72B0203010001";
-
-        protected override bool ValidateCertificate(byte[] certificateData)
+        [Serializable]
+        public class PlayerDeathEventLogEntry : PlayerBaseEventLogEntry
         {
-            return true;
+            public string target_ip_address;
+            public ulong target_steam_id;
+            public string target_name;
+            public float target_health;
+            public float target_heartrate;
+            public float target_location_x;
+            public float target_location_y;
+            public float target_location_z;
+            public bool hit_info_null;
+            public string weapon_name;
+            public bool did_gather;
+            public bool is_projectile;
+            public PlayerDeathEventLogEntry(BasePlayer player, HitInfo info) : base(player, "OnPlayerDeath")
+            {
+                if (info != null)
+                {
+                    hit_info_null = false;
+                    weapon_name = info.Weapon.name;
+                    did_gather = info.DidGather;
+                    is_projectile = info.IsProjectile();
+                }
+                else
+                {
+                    hit_info_null = true;
+                    weapon_name = "";
+                    did_gather = false;
+                    is_projectile = false;
+                }
+            }
+        }
 
-            X509Certificate2 certificate = new X509Certificate2(certificateData);
-            string pk = certificate.GetPublicKeyString();
-            if (pk.Equals(PUB_KEY))
+        [Serializable]
+        public class PlayerAttackEventLogEntry : PlayerBaseEventLogEntry
+        {
+            public string target_ip_address;
+            public ulong target_steam_id;
+            public string target_name;
+            public float target_health;
+            public float target_heartrate;
+            public float target_location_x;
+            public float target_location_y;
+            public float target_location_z;
+            public bool hit_info_null;
+            public string weapon_name;
+            public bool did_gather;
+            public bool is_projectile;
+            public PlayerAttackEventLogEntry(BasePlayer player, HitInfo info) : base(player, "OnPlayerAttack")
+            {
+
+                BasePlayer targetPlayer = info.HitEntity.GetComponent<BasePlayer>();
+
+                string playerAddress = targetPlayer.net.connection.ipaddress;
+                target_ip_address = playerAddress.Substring(0, playerAddress.LastIndexOf(":"));
+                target_steam_id = targetPlayer.userID;
+                target_name = targetPlayer.displayName;
+                target_health = targetPlayer.health;
+                target_heartrate = targetPlayer.metabolism.heartrate.lastValue;
+                target_location_x = targetPlayer.transform.position.x;
+                target_location_y = targetPlayer.transform.position.y;
+                target_location_z = targetPlayer.transform.position.z;
+
+                if (info != null)
+                {
+                    hit_info_null = false;
+                    weapon_name = info.Weapon.name;
+                    did_gather = info.DidGather;
+                    is_projectile = info.IsProjectile();
+                }
+                else
+                {
+                    hit_info_null = true;
+                    weapon_name = "";
+                    did_gather = false;
+                    is_projectile = false;
+                }
+            }
+        }
+
+        [Serializable]
+        public class PlayerLootEventLogEntry : PlayerBaseEventLogEntry
+        {
+
+            public string target_ip_address;
+            public ulong target_steam_id;
+            public string target_name;
+            public float target_health;
+            public float target_heartrate;
+            public float target_location_x;
+            public float target_location_y;
+            public float target_location_z;
+
+            public PlayerLootEventLogEntry(BasePlayer player, BasePlayer targetPlayer) : base(player, "OnLootPlayer")
+            {
+
+                string playerAddress = targetPlayer.net.connection.ipaddress;
+
+                target_ip_address = playerAddress.Substring(0, playerAddress.LastIndexOf(":"));
+                target_steam_id = targetPlayer.userID;
+                target_name = targetPlayer.displayName;
+                target_health = targetPlayer.health;
+                target_heartrate = targetPlayer.metabolism.heartrate.lastValue;
+                target_location_x = targetPlayer.transform.position.x;
+                target_location_y = targetPlayer.transform.position.y;
+                target_location_z = targetPlayer.transform.position.z;
+            }
+        }
+
+        [Serializable]
+        public class PlayerChatEventLogEntry : PlayerBaseEventLogEntry
+        {
+            public string message;
+            public PlayerChatEventLogEntry(BasePlayer player, string message) : base(player, "OnPlayerChat")
+            {
+                this.message = message;
+            }
+        }
+
+        [Serializable]
+        public class PlayerBaseEventLogEntry
+        {
+            // We're using snake_case because this will be serialized to JSON.
+            public int timestamp;
+            public string ip_address;
+            public ulong steam_id;
+            public string name;
+            public float health;
+            public float heartrate;
+            public float location_x;
+            public float location_y;
+            public float location_z;
+            public string hook_name;
+
+            public PlayerBaseEventLogEntry(BasePlayer player, string hookName)
+            {
+
+                string playerAddress = player.net.connection.ipaddress;
+
+                timestamp = getEpoch();
+                ip_address = playerAddress.Substring(0, playerAddress.LastIndexOf(":"));
+                steam_id = player.userID;
+                name = player.displayName;
+                health = player.health;
+                heartrate = player.metabolism.heartrate.lastValue;
+                location_x = player.transform.position.x;
+                location_y = player.transform.position.y;
+                location_z = player.transform.position.z;
+                hook_name = hookName;
+            }
+
+            int getEpoch()
+            {
+                TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+                return (int)t.TotalSeconds;
+            }
+        }
+
+        // Based on https://www.owasp.org/index.php/Certificate_and_Public_Key_Pinning#.Net
+        class AcceptPinnedCerts : CertificateHandler
+        {
+            // Encoded RSAPublicKey
+            private static string PUB_KEY = "30818902818100C4A06B7B52F8D17DC1CCB47362" +
+                "C64AB799AAE19E245A7559E9CEEC7D8AA4DF07CB0B21FDFD763C63A313A668FE9D764E" +
+                "D913C51A676788DB62AF624F422C2F112C1316922AA5D37823CD9F43D1FC54513D14B2" +
+                "9E36991F08A042C42EAAEEE5FE8E2CB10167174A359CEBF6FACC2C9CA933AD403137EE" +
+                "2C3F4CBED9460129C72B0203010001";
+
+            protected override bool ValidateCertificate(byte[] certificateData)
+            {
                 return true;
 
-            // Bad dog
-            return false;
+                X509Certificate2 certificate = new X509Certificate2(certificateData);
+                string pk = certificate.GetPublicKeyString();
+                if (pk.Equals(PUB_KEY))
+                    return true;
+
+                // Bad dog
+                return false;
+            }
         }
+
     }
 }
