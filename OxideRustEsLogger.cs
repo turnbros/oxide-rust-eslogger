fb3e48624da793d3ca86173d198c2895c1ff37ac
Dylan Turnbull <dylanturn@gmail.com>
2021-01-22 23:04:59 -0600

    added hooks for resource gathering

--- OxideRustEsLogger.cs
+++ OxideRustEsLogger.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using Newtonsoft.Json;
 using ConVar;
+using Oxide.Core.Libraries;
+using UnityEngine;
+using System.Linq;
 
 namespace Oxide.Plugins
 {
-    [Info("OxideRustEsLogger", "RedSys", 3.6)]
+    [Info("OxideRustEsLogger", "RedSys", 4.0)]
     [Description("Logs player actions.")]
     class OxideRustEsLogger : RustPlugin
     {
@@ -14,6 +17,42 @@ namespace Oxide.Plugins
                 LogToFile(fileName, JsonConvert.SerializeObject(eventObject), this);
             } catch (Exception error) {
                 LogToFile("error.log", $"[{DateTime.Now}] ERROR - {error.Message} - {error.StackTrace}", this);
+            }
+        }
+
+        void Loaded()
+        {
+            timer.Every(3f, () =>
+            {
+                var mapSize = TerrainMeta.Size.x;
+                var initialColliders = (int)(((mapSize * mapSize) / 1000000) * 1500);
+                int colliders = UnityEngine.Object.FindObjectsOfType<Collider>().Count(x => x.enabled);
+                CreateLogEntry("on_server_event.log", new ServerEventLogEntry(BasePlayer.activePlayerList.Count, Performance.current, colliders));
+            });
+        }
+        [Serializable]
+        public class ServerEventLogEntry : BaseEventLogEntry
+        {
+            public int player_count = 0;
+            public int fps = 0;
+            public int colliders = 0;
+            public long load_balancer_tasks = 0;
+            public long memory_allocations = 0;
+            public long memory_collections = 0;
+            public long memory_usage_system = 0;
+            public int ping = 0;
+
+            public ServerEventLogEntry() : base("ServerEvent"){}
+            public ServerEventLogEntry(int playerCount, Performance.Tick performance, int colliders) : base("ServerEvent")
+            {
+                player_count = playerCount;
+                fps = performance.frameRate;
+                this.colliders = colliders;
+                load_balancer_tasks = performance.loadBalancerTasks;
+                memory_allocations = performance.memoryAllocations;
+                memory_collections = performance.memoryCollections;
+                memory_usage_system = performance.memoryUsageSystem;
+                ping = performance.ping;
             }
         }
 
@@ -29,12 +68,13 @@ namespace Oxide.Plugins
             CreateLogEntry("on_player_attack.log", new ResidentAttack(attacker, info));
         }
         [Serializable]
-        public class ResidentAttack : BaseEventLogEntry
+        public class ResidentAttack : EntityEventLogEntry
         {
             public AggressiveAction aggressive_act;
             public ResidentAttack(BasePlayer player, HitInfo info) : base("OnPlayerAttack", player)
             {
-                aggressive_act = new AggressiveAction(info);
+                if (info == null) aggressive_act = new AggressiveAction();
+                else aggressive_act = new AggressiveAction(info);
             }
         }
 
@@ -47,12 +87,13 @@ namespace Oxide.Plugins
             return null;
         }
         [Serializable]
-        public class ResidentDead : BaseEventLogEntry
+        public class ResidentDead : EntityEventLogEntry
         {
             public AggressiveAction aggressive_act;
             public ResidentDead(BasePlayer player, HitInfo info) : base("OnPlayerDeath", player)
             {
-                aggressive_act = new AggressiveAction(info);
+                if(info == null) aggressive_act = new AggressiveAction();
+                else aggressive_act = new AggressiveAction(info);
             }
         }
 
@@ -64,7 +105,7 @@ namespace Oxide.Plugins
             CreateLogEntry("on_player_death.log", new ResidentLooted(player, target));
         }
         [Serializable]
-        public class ResidentLooted : BaseEventLogEntry
+        public class ResidentLooted : EntityEventLogEntry
         {
             public Resident looted_resident;
             public ResidentLooted(BasePlayer player, BasePlayer target) : base("OnLootPlayer", player)
@@ -86,7 +127,7 @@ namespace Oxide.Plugins
             CreateLogEntry("on_player_connect.log", new ResidentConnected(player));
         }
         [Serializable]
-        public class ResidentConnected : BaseEventLogEntry
+        public class ResidentConnected : EntityEventLogEntry
         {
             public ResidentConnected(BasePlayer player) : base("OnPlayerConnected", player){}
         }
@@ -99,7 +140,7 @@ namespace Oxide.Plugins
             CreateLogEntry("on_player_disconnect.log", new ResidentDisconnected(player, reason));
         }
         [Serializable]
-        public class ResidentDisconnected : BaseEventLogEntry
+        public class ResidentDisconnected : EntityEventLogEntry
         {
             public string reason;
             public ResidentDisconnected(BasePlayer player, string reason) : base("OnPlayerDisconnected", player)
@@ -108,7 +149,7 @@ namespace Oxide.Plugins
             }
         }
 
-
+        
         // On Player Chat
         // Called when the player sends chat to the server
         object OnPlayerChat(BasePlayer player, string message, Chat.ChatChannel channel)
@@ -117,7 +158,7 @@ namespace Oxide.Plugins
             return null;
         }
         [Serializable]
-        public class ResidentChatMessage : BaseEventLogEntry
+        public class ResidentChatMessage : EntityEventLogEntry
         {
             public string message;
             public string channel;
@@ -128,6 +169,42 @@ namespace Oxide.Plugins
             }
         }
 
+        // On Collectible Pickup
+        // Called when the player collects an item
+        object OnCollectiblePickup(Item item, BasePlayer player, CollectibleEntity entity)
+        {
+            CreateLogEntry("on_player_gather.log", new ResidentGatheredItem(player, entity?.name, item));
+            return null;
+        }
+
+        // On Growable Gathered
+        // Called before the player receives an item from gathering a growable entity
+        void OnGrowableGathered(GrowableEntity plant, Item item, BasePlayer player) {
+            CreateLogEntry("on_player_gather.log", new ResidentGatheredItem(player, plant?.name, item));
+        }
+
+        // On Dispenser Gather
+        // Called before the player is given items from a resource
+        object OnDispenserGather(ResourceDispenser dispenser, BaseEntity entity, Item item) {
+            CreateLogEntry("on_player_gather.log", new ResidentGatheredItem(entity, dispenser?.name, item));
+            return null; // Always return null!
+        }
+
+        // On Dispenser Bonus
+        // Called before the player is given a bonus item for gathering
+        object OnDispenserBonus(ResourceDispenser dispenser, BasePlayer player, Item item) {
+            CreateLogEntry("on_player_gather.log", new ResidentGatheredItem(player, dispenser?.name, item));
+            return null; // Always return null!
+        }
+
+        [Serializable]
+        public class ResidentGatheredItem : EntityEventLogEntry
+        {
+            public GatheredItemAction gathered_item_action;
+            public ResidentGatheredItem(BaseEntity entity, string dispenserName, Item item) : base("OnGather", entity){   
+                    gathered_item_action = new GatheredItemAction(dispenserName, item);
+            }
+        }
 
         /*****************************************
          ** Generic Serializable Object Classes **
@@ -135,17 +212,34 @@ namespace Oxide.Plugins
 
         [Serializable]
         public class BaseEventLogEntry {
-            public int timestamp;
-            public int log_format_version = 1;
-            public string event_name;
-            public Resident resident_subject;
-
-            public BaseEventLogEntry(string hook_name, BasePlayer player)
-            {
+            public static int GetTimestamp() {
                 TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
-                timestamp = (int)t.TotalSeconds;
-                event_name = hook_name;
-                resident_subject = new Resident(player);
+                return (int)t.TotalSeconds;
+            }
+            public int timestamp;
+            public int log_format_version = 2;
+            public string event_name;
+            public BaseEventLogEntry(string event_name) {
+                timestamp = GetTimestamp();
+                this.event_name = event_name;
+            }
+        }
+
+        [Serializable]
+        public class EntityEventLogEntry : BaseEventLogEntry {
+
+            public Entity resident_subject;
+            public Entity reporting_entity;
+
+            public EntityEventLogEntry(string event_name, BaseEntity entity) : base(event_name) {
+                BasePlayer player = entity?.GetComponent<BasePlayer>();
+                if (player != null) {
+                    reporting_entity = new Resident(player);
+                    resident_subject = new Resident(player);
+                } else {
+                    reporting_entity = new Entity(entity);
+                    resident_subject = new Entity(entity);
+                }
             }
         }
 
@@ -216,7 +310,30 @@ namespace Oxide.Plugins
             }
         }
 
-        
+        [Serializable]
+        public class GatheredItemAction
+        {
+            public string dispenser = "unknown";
+            public string name = "unknown";
+            public string category = "unknown";
+            public int amount = 0;
+
+            public GatheredItemAction() { }
+            public GatheredItemAction(string dispenserName, Item item)
+            {
+                if (item == null) return;
+
+                if (dispenserName != null)
+                    dispenser = dispenserName;
+
+                amount = item.amount;
+
+                if (item.info != null) {
+                    name = item.info.displayName.english;
+                    category = item.info.category.ToString();
+                }
+            }
+        }
 
         [Serializable]
         public class AggressiveAction
@@ -226,14 +343,22 @@ namespace Oxide.Plugins
             public string weapon_name = "unknown";
             public string weapon_prefab = "unknown";
             public string material_name = "unknown";
+
             public bool did_gather = false;
             public bool did_hit = false;
             public bool is_projectile = false;
             public bool is_headshot = false;
+            public bool is_attack = false;
+
+            public string damage_name = "unknown";
+            public string damage_type = "unknown";
+            public int damage_delt = 0;
+            public float projectile_distance = 0;
 
             public AggressiveAction() { }
             public AggressiveAction(HitInfo info)
             {
+                
                 if (info == null) return; 
 
                 // Get the source of the aggression
@@ -247,15 +372,15 @@ namespace Oxide.Plugins
 
 
                 // Get the target of the agression
-                BasePlayer player = info.HitEntity.GetComponent<BasePlayer>();
-                if (player != null)
+                BasePlayer player = info.HitEntity?.GetComponent<BasePlayer>();
+                if (player != null) {
                     aggression_target = new Resident(player);
-                else if(info.HitEntity != null)
+                } else if(info.HitEntity != null) {
                     aggression_target = new Entity(info.HitEntity);
+                }
 
-
-                if (info.Weapon?.ShortPrefabName != null)
-                {
+                // Get the weapons name
+                if (info.Weapon.ShortPrefabName != null) {
                     weapon_name = info.Weapon.ShortPrefabName;
                     weapon_prefab = info.Weapon.PrefabName;
                 }
@@ -263,9 +388,19 @@ namespace Oxide.Plugins
                 if (info.material != null)
                     material_name = info.material.name;
 
+                if (info.hasDamage) {
+                    damage_name = info.damageProperties.name;
+                    damage_type = info.damageTypes.GetMajorityDamageType().ToString();
+                    damage_delt = (int)info.damageTypes.Total();
+                    is_attack = info.damageTypes.IsConsideredAnAttack();
+                }
+
                 did_gather = info.DidGather;
-                is_projectile = info.IsProjectile();
                 is_headshot = info.isHeadshot;
+                if (info.IsProjectile()) {
+                    is_projectile = info.IsProjectile();
+                    projectile_distance = info.ProjectileDistance;
+                }
             }
         }
 
